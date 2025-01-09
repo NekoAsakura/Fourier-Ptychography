f6055be5279d84f3bd886f707b4bf70d11874bfb
Neko Asakura <neko.asakura@outlook.com>
2025-01-09 23:46:47 +0800

    package: use torch to speed up fft

--- Jupyter/utils.py
+++ Jupyter/utils.py
@@ -1,39 +1,60 @@
-import numpy as np
+import torch
 
 
 def get_args(
     m, n, aberr, array_size, wave_len, sp_size, p_size, NA, led_gap, led_height
 ):
+    device = torch.device("mps")
     scale = int(sp_size / p_size)
     m1 = m // scale
     n1 = n // scale
-    k0 = 2 * np.pi / wave_len
-    xloc, yloc = np.meshgrid(np.arange(array_size), np.arange(array_size))
-    xloc = ((xloc - (array_size - 1) / 2) * led_gap).ravel()
-    yloc = ((yloc - (array_size - 1) / 2) * led_gap).ravel()
-    kx_rel = -np.sin(np.arctan(xloc / led_height))
-    ky_rel = -np.sin(np.arctan(yloc / led_height))
+    k0 = 2 * torch.pi / wave_len
+
+    xloc, yloc = torch.meshgrid(
+        torch.arange(array_size, device=device),
+        torch.arange(array_size, device=device),
+        indexing="ij",
+    )
+    xloc = ((xloc - (array_size - 1) / 2) * led_gap).reshape(-1)
+    yloc = ((yloc - (array_size - 1) / 2) * led_gap).reshape(-1)
+
+    kx_rel = -torch.sin(torch.atan(xloc / led_height))
+    ky_rel = -torch.sin(torch.atan(yloc / led_height))
     kx = k0 * kx_rel
     ky = k0 * ky_rel
-    dkx = 2 * np.pi / (n * p_size)
-    dky = 2 * np.pi / (m * p_size)
+
+    dkx = 2 * torch.pi / (n * p_size)
+    dky = 2 * torch.pi / (m * p_size)
     cutoff = NA * k0
-    kmax = np.pi / sp_size
-    kxm, kym = np.meshgrid(np.linspace(-kmax, kmax, n1), np.linspace(-kmax, kmax, m1))
-    ctf = kxm**2 + kym**2 < cutoff**2
+    kmax = torch.pi / sp_size
+
+    kxm, kym = torch.meshgrid(
+        torch.linspace(-kmax, kmax, n1, device=device),
+        torch.linspace(-kmax, kmax, m1, device=device),
+        indexing="ij",
+    )
+
+    ctf = (kxm**2 + kym**2) < (cutoff**2)
+
     pupil = None
     if aberr is not None:
         z = aberr
-        kzm = np.sqrt(k0**2 - kxm**2 - kym**2)
-        pupil = np.exp(1j * z * np.real(kzm)) * np.exp(-abs(z) * np.abs(np.imag(kzm)))
-    return ctf, pupil, kx, ky, dkx, dky, m1, n1
+        kzm_sq = k0**2 - kxm**2 - kym**2
+        kzm = torch.sqrt(torch.clamp(kzm_sq, min=0.0)) + 1j * torch.sqrt(
+            torch.clamp(-kzm_sq, min=0.0)
+        )
+        pupil = torch.exp(1j * z * torch.real(kzm)) * torch.exp(
+            -abs(z) * torch.abs(torch.imag(kzm))
+        )
+
+    return ctf.float(), pupil, kx, ky, dkx, dky, m1, n1
 
 
 def gseq(array_size):
     assert array_size % 2 == 1, "NotImplemented: only support odd array_size yet."
-    n = (array_size + 1) / 2
-    seq = np.zeros((2, array_size**2), dtype=int)
-    seq[0][0], seq[1][0] = n, n
+    n = (array_size + 1) // 2
+    seq = torch.zeros((2, array_size**2), dtype=torch.int32)
+    seq[0, 0], seq[1, 0] = n, n
     dx, dy = +1, -1
     stepx, stepy = +1, -1
     direction = +1
